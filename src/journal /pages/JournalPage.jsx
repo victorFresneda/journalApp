45d635ab5eb09035e059585bccdb8304c9770ea1
victorFresneda <vafresnedah@hotmail.com>
2022-08-20 23:45:37 -0500

    [add] component Image Gallery

--- src/journal /pages/JournalPage.jsx
+++ src/journal /pages/JournalPage.jsx
@@ -1,4 +1,6 @@
 
+import { AddOutlined } from "@mui/icons-material";
+import { IconButton } from "@mui/material";
 import { JournalLayout } from "../layou/JournalLayout";
 import { NoteView, NothingSelecteView } from "../views";
 
@@ -7,8 +9,27 @@ export const JournalPage = () => {
     <JournalLayout>
     
 
-      {/* <NothingSelecteView /> */}
-      <NoteView />
+    <NothingSelecteView /> 
+      {/* <NoteView /> */}
+
+      <IconButton
+      size="large"
+
+      sx={{
+         color: "white",
+         backgroundColor: 'error.main',
+         ':hover': { backgroundColor: 'error.main', opacity: 0.7 },
+         position: 'fixed',
+         right: 50,
+         bottom: 50
+
+      }}
+      >
+      <AddOutlined sx={{ fonrSize: 30 }}/>
+
+      </IconButton>
+
+      
     </JournalLayout>
   );
 };
