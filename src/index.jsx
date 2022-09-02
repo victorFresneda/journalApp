6731ead3aa336978d275c9d676233ccb62ed7298
victorFresneda <vafresnedah@hotmail.com>
2022-09-01 23:59:12 -0500

    [add] redux is added

--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import { JournalApp } from './JournalApp';
-import './styles.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import { JournalApp } from "./JournalApp";
+import "./styles.css";
+import { store } from "./store";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-
-  <BrowserRouter>
-    <JournalApp />
-  </BrowserRouter>
-
+    <Provider store={ store }>
+      <BrowserRouter>
+        <JournalApp />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
-
-
---
+++ src/store/auth/authSlice.js
@@ -0,0 +1,20 @@
+
+
+
+import { createSlice } from '@reduxjs/toolkit';
+
+export const authSlice = createSlice({
+    name: 'auth',
+    initialState: {
+        counter: 10
+    },
+    reducers: {
+        increment: (state, /* action */ ) => {
+            state.counter += 1;
+        },
+    }
+});
+
+
+// Action creators are generated for each case reducer function
+export const { increment } = authSlice.actions;
